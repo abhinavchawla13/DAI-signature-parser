ca79fb4792ac1651110472225b83b61c06da1a5f
Abhinav Chawla <abhinavchawla13@gmail.com>
2019-06-12 02:48:51 -0400

    bug fix

--- extracter.py
+++ extracter.py
@@ -13,7 +13,7 @@ nlp = spacy.load('en_core_web_sm')
 # signature = _read_sig('emails/test0_clean.txt')
 
 signature_parsed = {}
-signature_unused = []
+signature = []
 
 WEBSITE_PATTERN = "((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
 EMAIL_PATTERN = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
@@ -135,7 +135,12 @@ def _extract_address(unparsed):
     
     return
 
-def extract(signature):
+def extract(sig):
+    global signature_parsed
+    global signature
+    signature_parsed = {}
+    signature = sig
+
     i = 0
     while (i < len(signature)):
         doc = nlp(signature[i])
